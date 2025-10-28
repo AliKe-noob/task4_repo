aed7f0dcb16276f27141587a5916dc532552f50d
AliKe <kenesbekovalmuhammed7@gmail.com>
2025-10-28 11:18:41 +0500

    Updated docstrings, added funcs for every process, validation check.

--- guess_basic.py
+++ guess_basic.py
@@ -1,19 +1,54 @@
 import random
 
-secret = random.randint(1, 100)
-print("I'm thinking of a number between 1-100")
+def generate_secret_number():
+    """
+    Generates and returns random number 1 - 100.
+    """
+    return random.randint(1, 100)
 
-while True:
-    try:
-        guess = int(input("Your guess: "))
-    except ValueError:
-        print("Out of range!")
-        continue
+def get_user_guess():
+    """
+    Prompts user to input a guess and handle invalids.
+
+    Returns the int for user guessed number.
+    """
+    while True:
+        try:
+            guess = int(input("Your guess: "))
+            return guess # Returns the valid input
+        except ValueError:
+            print("Out of range! Enter a valid Integer.")
         
+def provide_feedback(guess, secret):
+    """
+    Provide feedback based on user's guess.
+
+    Argumenta: 
+        guess for int and guessed number of user.
+        secret for int and to guess.
+    """
     if guess < secret:
         print("Too low! Try higher")
     elif guess > secret:
         print("Too high! Try lower")
     else:
         print(f"Correct! The number was {secret}")
-        break
+
+
+def guess_number():
+    """
+    main func to the game.
+    """
+    secret = generate_secret_number()
+    print("Im thinking of a number between 1 to 100.")
+    
+    while True:
+        guess = get_user_guess()
+        if guess == secret:
+            print (f"Correct! The number was {secret}.")
+            break
+        else:
+            provide_feedback(guess, secret)
+
+if __name__ == "__main__":
+    guess_number()
---
+++ q
@@ -0,0 +1,38 @@
+[33mcommit cbdc9c58e4892da317631833bc2e4facf8afa754[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m)[m
+Author: AliKe <kenesbekovalmuhammed7@gmail.com>
+Date:   Tue Oct 28 10:53:39 2025 +0500
+
+    Updated stats. Converts lowercase to upper, added strip
+
+[33mcommit 3dd41f71cda7d3be0935129b8470cd3d579823ef[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: AliKe <kenesbekovalmuhammed7@gmail.com>
+Date:   Tue Oct 28 10:43:08 2025 +0500
+
+    Removed duplication, clearer separations.
+    Fixed irregular indentations and format.
+    has name-main clause.
+    prevents bug if for validation properly.
+
+[33mcommit 286f87b6276e63b6c40f80352ee1cccc432ed69f[m
+Author: AliKe <kenesbekovalmuhammed7@gmail.com>
+Date:   Tue Oct 28 10:30:33 2025 +0500
+
+    added Try-except to handle non-integers, indentation
+
+[33mcommit 80a2621751f8ac7d2f6beba84045a3cadc2c8fe9[m
+Author: AliKe <kenesbekovalmuhammed7@gmail.com>
+Date:   Tue Oct 28 10:19:47 2025 +0500
+
+    guess_comp cleaned spaces
+
+[33mcommit ad123e7981ddb662de846e9a07f26fbe655a925e[m
+Author: AliKe <kenesbekovalmuhammed7@gmail.com>
+Date:   Tue Oct 28 09:37:50 2025 +0500
+
+    Put some stuff into README
+
+[33mcommit 36d51bd086757bca1331182ab15865210bd31943[m
+Author: AliKe <kenesbekovalmuhammed7@gmail.com>
+Date:   Tue Oct 28 09:33:04 2025 +0500
+
+    Put it on the watch
